dbbe7b8f1eb978fad3013d6b3adf77951528b1b8
ym0506 <atat9828@naver.com>
2025-03-30 16:23:32 +0900

    add

---
+++ 아아.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> graph(n + 1);
+    
+}
